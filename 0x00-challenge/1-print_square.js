61bf0d79b118bd4d7eb7f3c38cff4453f6a37285
miguel-dev <miguelacortes@zoho.com>
2020-04-03 03:58:53 +0000

    [Fixes] Removed var from size

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,7 +14,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-var size = parseInt(process.argv[2], 16)
+size = parseInt(process.argv[2], 16)
 
 for (var i = 0 ; i < size ; i ++) {
     for (var j = 0 ; j < (size*2) ; j ++) {

f8afdcfe0bd27abf4d28b8109faf5ab24dd2c98b
miguel-dev <miguelacortes@zoho.com>
2020-05-22 20:29:22 +0000

    [Feature] 1. My square

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+class square():
+
+    size = 0
+
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.size * 2
+
+    def PermiterOfMySquare(self):
+        return self.size * 4
+
+    def __str__(self):
+        return "{}/{}".format(self.size, self.size)
+
+if __name__ == "__main__":
+
+    s = square(size=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())

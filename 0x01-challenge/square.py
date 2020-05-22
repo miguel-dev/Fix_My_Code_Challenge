07e1758897dd71d06d428372d49a91fd58dfd6ab
miguel-dev <miguelacortes@zoho.com>
2020-05-22 21:41:15 +0000

    Added missing docstrings

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""Defines a square"""
 
 
 class square():
-
+    """Defines a square"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        "Initializes square class attributes"
         if (kwargs):
             values = kwargs.values()
             if (len(set(values)) == 1):
@@ -15,15 +17,16 @@ class square():
             else:
                 raise ValueError('width and height must be equal')
 
-
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """String representation of a Square"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":

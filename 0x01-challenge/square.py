b5e5926ef204801e5ef9e4c13d8b45a5600b15da
miguel-dev <miguelacortes@zoho.com>
2020-05-22 21:32:21 +0000

    [Fixes] Mantained the initial structure and added some verifications

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,25 +3,32 @@
 
 class square():
 
-    size = 0
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        if (kwargs):
+            values = kwargs.values()
+            if (len(set(values)) == 1):
+                for key, value in kwargs.items():
+                    setattr(self, key, value)
+            else:
+                raise ValueError('width and height must be equal')
+
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.size * 2
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        return self.size * 4
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.size, self.size)
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(size=9)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())

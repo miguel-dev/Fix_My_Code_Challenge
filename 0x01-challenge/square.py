b4873e2fa1c88af4a27eb15eeaeeb28d8387f403
miguel-dev <miguelacortes@zoho.com>
2020-05-22 22:31:29 +0000

    [Fixes] Capitalized the class name and fixed the syntax on the Permiter method

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """Square Module"""
 
 
-class square():
+class Square():
     """Defines a square"""
     width = 0
     height = 0
@@ -16,7 +16,7 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
